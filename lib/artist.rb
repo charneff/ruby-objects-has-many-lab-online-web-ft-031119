7c965cd656c73fe3176e5267815bf8eedcaa654b
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-01 15:19:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,19 @@
-class Artist 
-  
-  attr_accessor :name 
-  
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
-    @name = name 
-    @songs = []
+    @name = name
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
   def add_song(song)
-    @songs << song 
-    song.artist = self 
+    song.artist = self
   end
-  
-  def songs 
-    @songs 
-  end
-  
+
 end
